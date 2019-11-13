a52eda88a692241a3ccfc3dff9abb82f97bc11ac
Fardeem Munir <fardeem.munir@erubai.com>
2019-11-13 16:42:39 -0600

    Remove previous styles and replace with tailwind classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import React, { Component } from 'react';
-import reactLogo from './logo-react.svg';
-import djangoLogo from './logo-django.svg';
-import './App.css';
+import React, { Component } from "react";
+import reactLogo from "./logo-react.svg";
+import djangoLogo from "./logo-django.svg";
+import "./styles/index.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="bg-black h-screen">
+        <header className="flex text-white flex-wrap justify-center pt-4">
           <img src={reactLogo} className="App-logo" alt="React logo" />
           <img src={djangoLogo} className="App-logo" alt="Django logo" />
-          <h1 className="App-title">Welcome to the Dealdock app1</h1>
+          <h1 className="w-full text-center">Welcome to the Dealdock app</h1>
         </header>
-        <p className="App-intro">
-          I am a coding God.
-        </p>
-        <p>
-          change
-        </p>
       </div>
     );
   }
